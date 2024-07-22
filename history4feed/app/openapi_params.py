1dac600f16dc1a2a77eaf81d82210c9da7e35059
Fadl Olawumi <chaos@efqr.dev>
2024-07-22 09:35:45 +0100

    add errors to schema, closes #1

--- history4feed/app/openapi_params.py
+++ history4feed/app/openapi_params.py
@@ -4,16 +4,34 @@ from textwrap import dedent
 from .serializers import PostSerializer
 
 
-FEED_ID_PARAM = OpenApiParameter("feed_id", type=OpenApiTypes.UUID, description="The ID of the Feed. You can search for Feed IDs using the GET Feeds endpoints.", location=OpenApiParameter.PATH)
-JOB_ID_PARAM  = OpenApiParameter("job_id", type=OpenApiTypes.UUID, description="The ID of the Job. You can search for Job IDs using the GET Jobs endpoints.", location=OpenApiParameter.PATH)
-POST_ID_PARAM = OpenApiParameter("post_id", type=OpenApiTypes.UUID, description="The ID of the Post. You can search for Post IDs using the GET Posts endpoints for a specific Feed.", location=OpenApiParameter.PATH)
+FEED_ID_PARAM = OpenApiParameter(
+    "feed_id",
+    type=OpenApiTypes.UUID,
+    description="The ID of the Feed. You can search for Feed IDs using the GET Feeds endpoints.",
+    location=OpenApiParameter.PATH,
+)
+JOB_ID_PARAM = OpenApiParameter(
+    "job_id",
+    type=OpenApiTypes.UUID,
+    description="The ID of the Job. You can search for Job IDs using the GET Jobs endpoints.",
+    location=OpenApiParameter.PATH,
+)
+POST_ID_PARAM = OpenApiParameter(
+    "post_id",
+    type=OpenApiTypes.UUID,
+    description="The ID of the Post. You can search for Post IDs using the GET Posts endpoints for a specific Feed.",
+    location=OpenApiParameter.PATH,
+)
 
 
 XML_RESPONSE = OpenApiResponse(
     response=PostSerializer(many=True),
     description="",
     examples=[
-        OpenApiExample("xml", value=dedent("""
+        OpenApiExample(
+            "xml",
+            value=dedent(
+                """
             <?xml version="1.0" ?>
             <rss version="2.0">
                 <channel>
@@ -38,6 +56,11 @@ XML_RESPONSE = OpenApiResponse(
                 </item>
                 </channel>
             </rss>
-"""))
-    ]
+"""
+            ),
+        )
+    ],
 )
+
+HTTP404_EXAMPLE = OpenApiExample("http-404", {"message": "resource not found", "code": 404})
+HTTP400_EXAMPLE = OpenApiExample("http-400", {"message": "request not understood", "code": 400})
