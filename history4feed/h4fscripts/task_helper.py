muchdogesec/history4feed

commit bdf1782d7ffbc21872fa670e6fc6de00c5d0e95e

implement patch post (#27)

history4feed/h4fscripts/task_helper.py
@@ -17,8 +17,21 @@ def new_job(feed: models.Feed):
         earliest_item_requested=feed.latest_item_pubdate or settings.EARLIEST_SEARCH_DATE,
         latest_item_requested=datetime.now(),
     )
-    # earliest_entry = feed.latest_item_pubdate or settings.EARLIEST_SEARCH_DATE
     (start_job.s(job_obj.pk)| retrieve_posts_from_links.s(job_obj.pk) | wait_for_all_with_retry.s() | collect_and_schedule_removal.si(job_obj.pk)).apply_async(countdown=5)
+    return job_obj
+
+def new_patch_posts_job(feed: models.Feed, posts: list[models.Post]):
+    job_obj = models.Job.objects.create(
+        feed=posts[0].feed,
+        state=models.JobState.RUNNING,
+    )
+    ft_jobs = [models.FulltextJob.objects.create(
+        job_id=job_obj.id,
+        post_id=post.id,
+        link=post.link,
+    ) for post in posts]
+    chain = celery.chain([retrieve_full_text.si(ft_job.pk) for ft_job in ft_jobs])
+    ( chain | collect_and_schedule_removal.si(job_obj.pk)).apply_async()
     return job_obj
 
 @shared_task

history4feed/app/migrations/0009_alter_fulltextjob_post_alter_post_datetime_updated.py
@@ -1,0 +1,24 @@
+# Generated by Django 5.0.6 on 2024-08-28 14:41
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0008_fulltextjob_post_link'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='fulltextjob',
+            name='post',
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='app.post'),
+        ),
+        migrations.AlterField(
+            model_name='post',
+            name='datetime_updated',
+            field=models.DateTimeField(auto_now=True),
+        ),
+    ]

history4feed/app/models.py
@@ -86,7 +86,7 @@ class FullTextState(models.TextChoices):
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, help_text="UUID of items generated by history4feed")
     datetime_added = models.DateTimeField(auto_now_add=True, editable=False)
-    datetime_updated = models.DateTimeField(auto_now_add=True)
+    datetime_updated = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=1000, help_text="found in the <item> element of feed output")
     description = models.CharField(max_length=POST_DESCRIPTION_MAX_LENGTH, blank=True, help_text="found in the <item> element of feed output")
     link = models.URLField(max_length=1000, help_text="link to full article. found in the <item> element of feed output")
@@ -108,7 +108,7 @@ class Post(models.Model):
         self.categories.set(categories)
 
 class FulltextJob(models.Model):
-    post = models.OneToOneField(Post, on_delete=models.SET_NULL, null=True)
+    post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
     job = models.ForeignKey(Job, related_name="fulltext_jobs", on_delete=models.CASCADE)
     status = models.CharField(max_length=15, choices=FullTextState.choices, default=FullTextState.RETRIEVING)
     error_str = models.CharField(max_length=1500, null=True, blank=True)

history4feed/app/views.py
@@ -77,6 +77,17 @@ class ErrorResp(Response):
 
 
 # Create your views here.
+@extend_schema_view(
+    partial_update=extend_schema(
+        description="fetch update for post",
+        summary="fetch update for post",
+        responses={
+            200: JobSerializer,
+            404: OpenApiResponse(H4FError, "Feed or post does not exist", examples=[HTTP404_EXAMPLE]),
+        },
+        tags=["Feeds"],
+    ),
+)
 class PostView(
     mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
 ):
@@ -168,6 +179,18 @@ class PostView(
     )
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
+    
+
+    def partial_update(self, request, *args, **kwargs):
+        post: Post = self.get_object()
+        job_obj = task_helper.new_patch_posts_job(post.feed, [post])
+        job_resp = {
+            "datetime_added": post.datetime_added,
+            "job_state": job_obj.state,
+            "id": post.id,
+            "job_id": job_obj.id,
+        }
+        return Response(job_resp)
 
 
 class FeedView(viewsets.ModelViewSet):

commit 5933e0c5aa4510e93919c478ea19c2bc3f9ec30d

fix regression caused by #27

history4feed/h4fscripts/task_helper.py
@@ -68,8 +68,10 @@ def retrieve_posts_from_links(urls, job_id):
     for index, url in enumerate(urls):
         parsed_feed, posts, error = retrieve_posts_from_url(url, feed, job_id)
         if error:
+            logger.exception(error)
             continue
         if not posts:
+            logger.warning('no new post in `%s`', url)
             continue
 
         chain_tasks = []
@@ -126,7 +128,7 @@ def retrieve_posts_from_url(url, db_feed: models.Feed, job_id: str):
                     continue
                 categories = post_dict.categories
                 del post_dict.categories
-                post, created = models.Post.objects.get_or_create(defaults={**post_dict.__dict__, "job_id":job_id}, feed=db_feed, link=post_dict.link)
+                post, created = models.Post.objects.get_or_create(defaults=post_dict.__dict__, feed=db_feed, link=post_dict.link)
                 if not created:
                     continue
                 db_feed.earliest_item_pubdate = min(db_feed.earliest_item_pubdate or post.pubdate, post.pubdate)