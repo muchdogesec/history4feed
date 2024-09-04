muchdogesec/history4feed

commit bdf1782d7ffbc21872fa670e6fc6de00c5d0e95e

implement patch post (#27)

history4feed/app/migrations/0009_alter_fulltextjob_post_alter_post_datetime_updated.py
@@ -1,0 +1,24 @@
+# Generated by Django 5.0.6 on 2024-08-28 14:41
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0008_fulltextjob_post_link'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='fulltextjob',
+            name='post',
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='app.post'),
+        ),
+        migrations.AlterField(
+            model_name='post',
+            name='datetime_updated',
+            field=models.DateTimeField(auto_now=True),
+        ),
+    ]

history4feed/app/models.py
@@ -86,7 +86,7 @@ class FullTextState(models.TextChoices):
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, help_text="UUID of items generated by history4feed")
     datetime_added = models.DateTimeField(auto_now_add=True, editable=False)
-    datetime_updated = models.DateTimeField(auto_now_add=True)
+    datetime_updated = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=1000, help_text="found in the <item> element of feed output")
     description = models.CharField(max_length=POST_DESCRIPTION_MAX_LENGTH, blank=True, help_text="found in the <item> element of feed output")
     link = models.URLField(max_length=1000, help_text="link to full article. found in the <item> element of feed output")
@@ -108,7 +108,7 @@ class Post(models.Model):
         self.categories.set(categories)
 
 class FulltextJob(models.Model):
-    post = models.OneToOneField(Post, on_delete=models.SET_NULL, null=True)
+    post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
     job = models.ForeignKey(Job, related_name="fulltext_jobs", on_delete=models.CASCADE)
     status = models.CharField(max_length=15, choices=FullTextState.choices, default=FullTextState.RETRIEVING)
     error_str = models.CharField(max_length=1500, null=True, blank=True)

history4feed/app/views.py
@@ -77,6 +77,17 @@ class ErrorResp(Response):
 
 
 # Create your views here.
+@extend_schema_view(
+    partial_update=extend_schema(
+        description="fetch update for post",
+        summary="fetch update for post",
+        responses={
+            200: JobSerializer,
+            404: OpenApiResponse(H4FError, "Feed or post does not exist", examples=[HTTP404_EXAMPLE]),
+        },
+        tags=["Feeds"],
+    ),
+)
 class PostView(
     mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
 ):
@@ -168,6 +179,18 @@ class PostView(
     )
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
+    
+
+    def partial_update(self, request, *args, **kwargs):
+        post: Post = self.get_object()
+        job_obj = task_helper.new_patch_posts_job(post.feed, [post])
+        job_resp = {
+            "datetime_added": post.datetime_added,
+            "job_state": job_obj.state,
+            "id": post.id,
+            "job_id": job_obj.id,
+        }
+        return Response(job_resp)
 
 
 class FeedView(viewsets.ModelViewSet):

history4feed/h4fscripts/task_helper.py
@@ -17,8 +17,21 @@ def new_job(feed: models.Feed):
         earliest_item_requested=feed.latest_item_pubdate or settings.EARLIEST_SEARCH_DATE,
         latest_item_requested=datetime.now(),
     )
-    # earliest_entry = feed.latest_item_pubdate or settings.EARLIEST_SEARCH_DATE
     (start_job.s(job_obj.pk)| retrieve_posts_from_links.s(job_obj.pk) | wait_for_all_with_retry.s() | collect_and_schedule_removal.si(job_obj.pk)).apply_async(countdown=5)
+    return job_obj
+
+def new_patch_posts_job(feed: models.Feed, posts: list[models.Post]):
+    job_obj = models.Job.objects.create(
+        feed=posts[0].feed,
+        state=models.JobState.RUNNING,
+    )
+    ft_jobs = [models.FulltextJob.objects.create(
+        job_id=job_obj.id,
+        post_id=post.id,
+        link=post.link,
+    ) for post in posts]
+    chain = celery.chain([retrieve_full_text.si(ft_job.pk) for ft_job in ft_jobs])
+    ( chain | collect_and_schedule_removal.si(job_obj.pk)).apply_async()
     return job_obj
 
 @shared_task

commit 360f92cc2763157ba77df81813d9f8efc37c9c8e

Patch post impl (#30)
* implement patch post (#27)

* generate post.id using uuid5 (#28)

* updating some text for Swagger view

* add feed_id to id generation (#28)

* remove conditin causing no retrieval; change job_id filter (#27)

* adding tests

* Update test_01_check_feed_types.py

* test still broken

* fix regression caused by #27

* fixing broken test

* remove request body from patch posts (#29)

---------

Co-authored-by: Fadl <chaos@efqr.dev>

history4feed/app/migrations/0009_alter_fulltextjob_post_alter_post_datetime_updated.py
@@ -1,0 +1,24 @@
+# Generated by Django 5.0.6 on 2024-08-28 14:41
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0008_fulltextjob_post_link'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='fulltextjob',
+            name='post',
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='app.post'),
+        ),
+        migrations.AlterField(
+            model_name='post',
+            name='datetime_updated',
+            field=models.DateTimeField(auto_now=True),
+        ),
+    ]

history4feed/app/migrations/0010_alter_post_id.py
@@ -1,0 +1,18 @@
+# Generated by Django 5.0.6 on 2024-08-29 16:37
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0009_alter_fulltextjob_post_alter_post_datetime_updated'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='post',
+            name='id',
+            field=models.UUIDField(help_text='UUID of items generated by history4feed', primary_key=True, serialize=False),
+        ),
+    ]

history4feed/app/migrations/0011_remove_post_job_alter_fulltextjob_post.py
@@ -1,0 +1,23 @@
+# Generated by Django 5.0.6 on 2024-09-02 08:36
+
+import django.db.models.deletion
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('app', '0010_alter_post_id'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='post',
+            name='job',
+        ),
+        migrations.AlterField(
+            model_name='fulltextjob',
+            name='post',
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='fulltext_jobs', to='app.post'),
+        ),
+    ]

history4feed/app/models.py
@@ -29,6 +29,11 @@ class SlugField(models.CharField):
 class Category(models.Model):
     name = SlugField(max_length=1000, primary_key=True)
 
+
+def stix_id(url):
+    return uuid.uuid5(uuid.UUID(settings.HISTORY4FEED_NAMESPACE), url)
+    
+
 class Feed(models.Model):
     id = models.UUIDField(primary_key=True, help_text="UUID of feed generated by history4feed")
     title = models.CharField(max_length=1000, help_text="found in the <channel> of RSS output. Is always kept up to date with the latest feed import values for this property.")
@@ -49,12 +54,9 @@ class Feed(models.Model):
     
     def save(self, *args, **kwargs) -> None:
         if not self.id:
-            self.id = self.stix_id(self.url)
+            self.id = stix_id(self.url)
         return super().save(*args, **kwargs)
     
-    @staticmethod
-    def stix_id(url):
-        return uuid.uuid5(uuid.UUID(settings.HISTORY4FEED_NAMESPACE), url)
     
     def should_skip_post(self, post_link: str):
         return (not self.include_remote_blogs) and urlparse(self.url).hostname.split('.')[-2:] != urlparse(post_link).hostname.split('.')[-2:]
@@ -84,9 +86,9 @@ class FullTextState(models.TextChoices):
     RETRIEVING = "retrieving"
 
 class Post(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, help_text="UUID of items generated by history4feed")
+    id = models.UUIDField(primary_key=True, help_text="UUID of items generated by history4feed")
     datetime_added = models.DateTimeField(auto_now_add=True, editable=False)
-    datetime_updated = models.DateTimeField(auto_now_add=True)
+    datetime_updated = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=1000, help_text="found in the <item> element of feed output")
     description = models.CharField(max_length=POST_DESCRIPTION_MAX_LENGTH, blank=True, help_text="found in the <item> element of feed output")
     link = models.URLField(max_length=1000, help_text="link to full article. found in the <item> element of feed output")
@@ -94,7 +96,6 @@ class Post(models.Model):
     author = models.CharField(max_length=1000, help_text="author of the post")
     categories = models.ManyToManyField(Category, related_name="posts", help_text="categories of the post")
     feed = models.ForeignKey(Feed, on_delete=models.CASCADE, related_name="posts", help_text="feed id this item belongs too")
-    job = models.ForeignKey(Job, on_delete=models.SET_NULL, null=True)
     is_full_text = models.BooleanField(default=False, help_text="if full text has been retrieved")
     content_type = models.CharField(default="plain/text", max_length=200, help_text="content type of the description")
 
@@ -107,8 +108,15 @@ class Post(models.Model):
         categories = [Category.objects.get_or_create(name=name)[0] for name in categories]
         self.categories.set(categories)
 
+
+    def save(self, *args, **kwargs) -> None:
+        if not self.id:
+            pubdate = self.pubdate.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+            self.id = stix_id(f"{self.feed.id}+{self.link}+{pubdate}")
+        return super().save(*args, **kwargs)
+
 class FulltextJob(models.Model):
-    post = models.OneToOneField(Post, on_delete=models.SET_NULL, null=True)
+    post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True, related_name="fulltext_jobs")
     job = models.ForeignKey(Job, related_name="fulltext_jobs", on_delete=models.CASCADE)
     status = models.CharField(max_length=15, choices=FullTextState.choices, default=FullTextState.RETRIEVING)
     error_str = models.CharField(max_length=1500, null=True, blank=True)

history4feed/app/serializers.py
@@ -13,7 +13,7 @@ class FeedSerializer(serializers.ModelSerializer):
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        exclude = ['feed', 'job']
+        exclude = ['feed']
         # fields = '__all__'
 
 class JobUrlStatusSerializer(serializers.Serializer):

history4feed/app/views.py
@@ -77,6 +77,18 @@ class ErrorResp(Response):
 
 
 # Create your views here.
+@extend_schema_view(
+    partial_update=extend_schema(
+        description="Occasionally updates to blog posts are not reflected in RSS and ATOM feeds. To ensure the post stored in history4feed matches the currently published post you make a request to this endpoint using the Post ID to update it.",
+        summary="Update a Post in a Feed",
+        responses={
+            200: JobSerializer,
+            404: OpenApiResponse(H4FError, "Feed or post does not exist", examples=[HTTP404_EXAMPLE]),
+        },
+        tags=["Feeds"],
+        request=None,
+    ),
+)
 class PostView(
     mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
 ):
@@ -99,7 +111,7 @@ class PostView(
             label="Filter by the content in a posts description. Will search for descriptions that contain the value entered.",
             lookup_expr="search",
         )
-        job_id = Filter(label="Filter the Post by Job ID the Post was downloaded in.")
+        job_id = Filter(label="Filter the Post by Job ID the Post was downloaded in.", field_name="fulltext_jobs__job_id")
 
     def get_queryset(self):
         return Post.objects.filter(feed_id=self.kwargs.get("feed_id"))
@@ -168,6 +180,19 @@ class PostView(
     )
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
+    
+
+    def partial_update(self, request, *args, **kwargs):
+        post: Post = self.get_object()
+        job_obj = task_helper.new_patch_posts_job(post.feed, [post])
+        job_resp = {
+            "datetime_added": post.datetime_added,
+            "job_state": job_obj.state,
+            "post_id": post.id,
+            "feed_id": post.feed.id,
+            "id": job_obj.id,
+        }
+        return Response(job_resp)
 
 
 class FeedView(viewsets.ModelViewSet):
@@ -248,7 +273,8 @@ class FeedView(viewsets.ModelViewSet):
         request=None,
         description=textwrap.dedent(
             """
-        Use this endpoint to check for new posts on this blog since the last update time. An update request will immediately trigger a job to get the posts between `latest_item_pubdate` for feed and time you make a request to this endpoint.
+        Use this endpoint to check for new posts on this blog since the last update time. An update request will immediately trigger a job to get the posts between `latest_item_pubdate` for feed and time you make a request to this endpoint.\n\n
+        Note, this endpoint can miss updates to currently indexed posts (where the RSS or ATOM feed does not report the updated correctly -- which is very common). To solve this issue for currently indexed blog posts, use the Update Post endpoint.
         """
         ),
         tags=open_api_tags,

history4feed/h4fscripts/task_helper.py
@@ -17,8 +17,21 @@ def new_job(feed: models.Feed):
         earliest_item_requested=feed.latest_item_pubdate or settings.EARLIEST_SEARCH_DATE,
         latest_item_requested=datetime.now(),
     )
-    # earliest_entry = feed.latest_item_pubdate or settings.EARLIEST_SEARCH_DATE
     (start_job.s(job_obj.pk)| retrieve_posts_from_links.s(job_obj.pk) | wait_for_all_with_retry.s() | collect_and_schedule_removal.si(job_obj.pk)).apply_async(countdown=5)
+    return job_obj
+
+def new_patch_posts_job(feed: models.Feed, posts: list[models.Post]):
+    job_obj = models.Job.objects.create(
+        feed=posts[0].feed,
+        state=models.JobState.RUNNING,
+    )
+    ft_jobs = [models.FulltextJob.objects.create(
+        job_id=job_obj.id,
+        post_id=post.id,
+        link=post.link,
+    ) for post in posts]
+    chain = celery.chain([retrieve_full_text.si(ft_job.pk) for ft_job in ft_jobs])
+    ( chain | collect_and_schedule_removal.si(job_obj.pk)).apply_async()
     return job_obj
 
 @shared_task
@@ -55,8 +68,10 @@ def retrieve_posts_from_links(urls, job_id):
     for index, url in enumerate(urls):
         parsed_feed, posts, error = retrieve_posts_from_url(url, feed, job_id)
         if error:
+            logger.exception(error)
             continue
         if not posts:
+            logger.warning('no new post in `%s`', url)
             continue
 
         chain_tasks = []
@@ -113,7 +128,7 @@ def retrieve_posts_from_url(url, db_feed: models.Feed, job_id: str):
                     continue
                 categories = post_dict.categories
                 del post_dict.categories
-                post, created = models.Post.objects.get_or_create(defaults={**post_dict.__dict__, "job_id":job_id}, feed=db_feed, link=post_dict.link)
+                post, created = models.Post.objects.get_or_create(defaults=post_dict.__dict__, feed=db_feed, link=post_dict.link)
                 if not created:
                     continue
                 db_feed.earliest_item_pubdate = min(db_feed.earliest_item_pubdate or post.pubdate, post.pubdate)
@@ -139,8 +154,7 @@ def retrieve_posts_from_url(url, db_feed: models.Feed, job_id: str):
 def retrieve_full_text(self, ftjob_pk):
     fulltext_job = models.FulltextJob.objects.get(pk=ftjob_pk)
     try:
-        if not fulltext_job.post.is_full_text:
-            fulltext_job.post.description, fulltext_job.post.content_type = h4f.get_full_text(fulltext_job.post.link)
+        fulltext_job.post.description, fulltext_job.post.content_type = h4f.get_full_text(fulltext_job.post.link)
         fulltext_job.status = models.FullTextState.RETRIEVED
         fulltext_job.error_str = ""
         fulltext_job.post.is_full_text = True

tests/base_test.py
@@ -1,0 +1,96 @@
+import unittest
+
+class BaseTest(unittest.TestCase):
+    def setUp(self):
+        # Define the base URL for the API requests
+        self.base_url = "http://127.0.0.1:8002/api/v1/feeds/"
+
+        # Feeds URLs, their corresponding feed IDs, descriptions, and feed types
+        self.feeds = {
+            "https://muchdogesec.github.io/fakeblog123/feeds/rss-feed-encoded.xml": {
+                "id": "d1d96b71-c687-50db-9d2b-d0092d1d163a",
+                "description": "RSS -- Contains encoded html -- FULL CONTENT",
+                "feed_type": "rss"
+            },
+            "https://muchdogesec.github.io/fakeblog123/feeds/rss-feed-decoded.xml": {
+                "id": "8f89731d-b9de-5931-9182-5460af59ca84",
+                "description": "RSS -- Contains decoded html (without CDATA tags, but it XML escaped) -- FULL CONTENT",
+                "feed_type": "rss"
+            },
+            "https://muchdogesec.github.io/fakeblog123/feeds/rss-feed-cdata.xml": {
+                "id": "c8592fca-aa7b-55b7-9664-886230d7c338",
+                "description": "RSS -- Contains decoded html inside CDATA tags -- FULL CONTENT",
+                "feed_type": "rss"
+            },
+            "https://muchdogesec.github.io/fakeblog123/feeds/rss-feed-encoded-partial.xml": {
+                "id": "e6178850-0b78-54cc-9f3e-85b482b84f2b",
+                "description": "RSS -- Contains encoded html -- PARTIAL CONTENT ONLY",
+                "feed_type": "rss"
+            },
+            "https://muchdogesec.github.io/fakeblog123/feeds/rss-feed-decoded-partial.xml": {
+                "id": "8375e600-cc52-5823-8179-a8313ba9df5c",
+                "description": "RSS -- Contains decoded html (without CDATA tags, but it XML escaped) -- PARTIAL CONTENT ONLY",
+                "feed_type": "rss"
+            },
+            "https://muchdogesec.github.io/fakeblog123/feeds/rss-feed-cdata-partial.xml": {
+                "id": "2d6575b8-3d90-5479-bdfe-b980b753ec40",
+                "description": "RSS -- Contains decoded html inside CDATA tags -- PARTIAL CONTENT ONLY",
+                "feed_type": "rss"
+            },
+            "https://muchdogesec.github.io/fakeblog123/feeds/atom-feed-encoded.xml": {
+                "id": "2f21dfd2-e776-5d2b-ad3d-00460e540cca",
+                "description": "ATOM -- Contains encoded html -- FULL CONTENT",
+                "feed_type": "atom"
+            },
+            "https://muchdogesec.github.io/fakeblog123/feeds/atom-feed-decoded.xml": {
+                "id": "cb0ba709-b841-521a-a3f2-5e1429f4d366",
+                "description": "ATOM -- Contains decoded html (without CDATA tags, but it XML escaped) -- FULL CONTENT",
+                "feed_type": "atom"
+            },
+            "https://muchdogesec.github.io/fakeblog123/feeds/atom-feed-cdata.xml": {
+                "id": "121e5557-7277-5aa3-945d-e466c6bf92d5",
+                "description": "ATOM -- Contains decoded html inside CDATA tags -- FULL CONTENT",
+                "feed_type": "atom"
+            },
+            "https://muchdogesec.github.io/fakeblog123/feeds/atom-feed-encoded-partial.xml": {
+                "id": "66966023-522a-57af-93ac-88c6214e1891",
+                "description": "ATOM -- Contains encoded html -- PARTIAL CONTENT ONLY",
+                "feed_type": "atom"
+            },
+            "https://muchdogesec.github.io/fakeblog123/feeds/atom-feed-decoded-partial.xml": {
+                "id": "9c04d319-a949-52df-bcb6-5a73a1458fe5",
+                "description": "ATOM -- Contains decoded html (without CDATA tags, but it XML escaped) -- PARTIAL CONTENT ONLY",
+                "feed_type": "atom"
+            },
+            "https://muchdogesec.github.io/fakeblog123/feeds/atom-feed-cdata-partial.xml": {
+                "id": "220ae197-f66f-56b3-a17a-cbfc6dc9661a",
+                "description": "ATOM -- Contains decoded html inside CDATA tags -- PARTIAL CONTENT ONLY",
+                "feed_type": "atom"
+            }
+        }
+
+        # Posts corresponding to feed IDs
+        self.posts = {
+            "d1d96b71-c687-50db-9d2b-d0092d1d163a": [
+                {
+                    "id": "8f16d2be-7b06-5f3c-a851-9cce31b4fec8",
+                    "title": "Update this post for testing updates to posts",
+                    "pubdate": "2024-08-20T10:00:00Z"
+                },
+                {
+                    "id": "47f72d92-f22a-5f08-84f3-55aedf4c7967",
+                    "title": "Testing Extractions",
+                    "pubdate": "2024-08-07T08:00:00Z"
+                },
+                {
+                    "id": "85a762c9-00f9-5c0c-9858-498883e13ea1",
+                    "title": "Testing Markdown Elements",
+                    "pubdate": "2024-08-05T08:00:00Z"
+                }, 
+                {
+                    "id": "29be2407-d5d1-5b47-bbb5-1c51a84d48eb",
+                    "title": "Real Post Example - Fighting Ursa Luring Targets With Car for Sale",
+                    "pubdate": "2024-08-01T08:00:00Z"
+                }
+            ]
+        }

tests/test_01_add_feeds_check_ids.py
@@ -1,0 +1,82 @@
+import unittest
+import requests
+import time
+from .base_test import BaseTest  # Ensure this import path is correct
+
+class TestFeedProcessing(BaseTest):
+
+    def delete_existing_feeds(self):
+        """Deletes existing feeds from the API to ensure a clean state."""
+        print("Starting deletion of existing feeds...")
+        for feed_id in [details['id'] for details in self.feeds.values()]:
+            url = f"{self.base_url}{feed_id}/"
+            while True:
+                response = requests.delete(url)
+                print(f"DELETE {url} - Status Code: {response.status_code}")
+                if response.status_code == 404:
+                    print(f"Feed ID {feed_id} already deleted or not found.")
+                    break
+                elif response.status_code == 204:
+                    print(f"Feed ID {feed_id} deleted successfully.")
+                else:
+                    print(f"Unexpected status code {response.status_code} when deleting feed ID {feed_id}.")
+                    break
+
+    def setUp(self):
+        """Ensures that the DELETE operation runs before any test starts."""
+        super().setUp()
+        self.delete_existing_feeds()
+
+    def test_post_feed_urls_and_check_jobs(self):
+        """Tests adding feeds via POST requests, checks job status until success, and verifies post retrieval."""
+        job_ids = []
+
+        # Step 1: Post feeds and collect job IDs
+        for feed_url in self.feeds.keys():
+            with self.subTest(feed_url=feed_url):
+                response = requests.post(
+                    self.base_url,
+                    json={
+                        "url": feed_url,
+                        "include_remote_blogs": False
+                    },
+                    headers={"Accept": "application/json"}  # Ensure the server responds with JSON
+                )
+                print(f"POST {self.base_url} - Feed URL: {feed_url} - Status Code: {response.status_code}")
+                self.assertEqual(response.status_code, 200, f"Request to {self.base_url} failed with status code {response.status_code}")
+                data = response.json()
+                job_id = data.get("job_id")
+                self.assertIsNotNone(job_id, f"Job ID not returned for URL {feed_url}")
+                job_ids.append(job_id)
+
+        # Step 2: Check the job status until it reaches "success"
+        print("Checking job status until success...")
+        for job_id in job_ids:
+            job_url = f"http://localhost:8002/api/v1/jobs/{job_id}/"
+            max_attempts = 20
+            for attempt in range(max_attempts):
+                response = requests.get(job_url, headers={"Accept": "application/json"})
+                print(f"GET {job_url} - Status Code: {response.status_code} - Attempt {attempt + 1}/{max_attempts}")
+                self.assertEqual(response.status_code, 200, f"Request to {job_url} failed with status code {response.status_code}")
+                data = response.json()
+                state = data.get("state")
+                print(f"Job ID {job_id} - State: {state}")
+                if state == "success":
+                    break
+                elif attempt < max_attempts - 1:
+                    time.sleep(4)  # Wait before the next check
+                else:
+                    self.fail(f"Job ID {job_id} did not reach 'success' state after {max_attempts} attempts.")
+
+        # Step 3: Verify that each post can be retrieved with a 200 OK response
+        print("Verifying retrieval of posts...")
+        for feed_id, posts in self.posts.items():
+            for post in posts:
+                post_url = f"http://localhost:8002/api/v1/feeds/{feed_id}/posts/{post['id']}/"
+                response = requests.get(post_url, headers={"Accept": "application/json"})
+                print(f"GET {post_url} - Status Code: {response.status_code}")
+                self.assertEqual(response.status_code, 200, f"Request to {post_url} failed with status code {response.status_code}")
+
+# To run the tests
+if __name__ == '__main__':
+    unittest.main()