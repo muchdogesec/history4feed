e5065fbffaf5c9e4c5330540a829b514f4c6311a
Ridwan Olawumi <olarid7852@gmail.com>
2024-11-05 11:12:48 +0100

    Created staging and production github action

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 python manage.py migrate
-gunicorn history4feed.wsgi:application --bind 0.0.0.0:8002 --reload
+#gunicorn history4feed.wsgi:application --bind 0.0.0.0:8002 --reload
+python manage.py runserver 0.0.0.0:8002
