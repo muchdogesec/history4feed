d9ec200aba9daf3235b90f59550f26a89c43a2c2
Chaos Pjeles <chaos@efqr.dev>
2026-02-12 12:06:56 +0100

    fix unconventional validation error message when duplicate link exist on feed (#207)

---
+++ tests/src/test_serializers.py
@@ -0,0 +1,21 @@
+from history4feed.app.serializers import CreatePostsSerializer
+from datetime import datetime as dt, UTC
+import pytest
+
+@pytest.mark.django_db
+def test_create_posts_serializer__existing_url(feed_posts):
+    feed, (post, _) = feed_posts
+    serializer = CreatePostsSerializer(data={
+        "posts": [
+            {
+                "title": "New Post",
+                "link": post.link,  # Same link as existing post
+                "pubdate": post.pubdate.isoformat(),
+                "author": "Author",
+                "categories": ["Category1", "Category2"],
+            }
+        ]
+    }, context={'feed_id': feed.id})
+    assert not serializer.is_valid()
+    assert 'link' in serializer.errors['posts'][0]
+    assert serializer.errors['posts'][0]['link'][0] == f'Post at `{post.link}` already exists in feed.'
